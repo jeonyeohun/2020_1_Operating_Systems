185637241adf9bca39c30d3159b1f40f277d332c
jeonyeohun <21500630@handong.edu>
2020-05-22 18:12:12 +0900

    implement initial approach

--- PA3/mtsp.c
+++ PA3/mtsp.c
@@ -6,25 +6,77 @@
 #include <limits.h>
 #include <memory.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define MAX_SUBTASK 11            
 
-int cities[51][51];                 // Map of city distance
-int visited [51] = {0,};            // Mark visited city
-int path [51] = {0,};               // Record route
+int cities[51][51];                 // Map of city distance               // Record route
 int minPath [51] = {0,};            // The best path for the shortest distance
 int size;                           // The total number of cities
-int childNum = 0;                   // Tracking the number of child process currently running
-
 int length;                         // Current weight of distance
 int min = -1;                       // Store minimum distance of traversed route
+int threadLimit;
 
 long long checkedRoute=0;           // Number of checked route by single process
 
-int buffer[100];
-int in = -1;
-int out = -1;
+typedef struct {
+	sem_t filled ;
+	sem_t empty ;
+	pthread_mutex_t lock ;
+	int ** elem ;
+	int capacity ;
+	int num ; 
+	int front ;
+	int rear ;
+} bounded_buffer ;
 
+bounded_buffer * buf = 0x0 ;
+
+void bounded_buffer_init(bounded_buffer * buf, int capacity) {
+	sem_init(&(buf->filled), 0, 0) ;
+	sem_init(&(buf->empty), 0, capacity) ;
+	pthread_mutex_init(&(buf->lock), 0x0) ;
+	buf->capacity = capacity ;
+	buf->elem = (int **) calloc(sizeof(int *), capacity) ;
+	buf->num = 0 ;
+	buf->front = 0 ;
+	buf->rear = 0 ;
+}
+
+void bounded_buffer_queue(bounded_buffer * buf, int * msg, int idx) 
+{
+	sem_wait(&(buf->empty)) ;
+	pthread_mutex_lock(&(buf->lock)) ;
+        (buf->elem)[buf->rear] = (int *) malloc (sizeof(msg[0]) * idx);
+        memcpy((buf->elem)[buf->rear], msg, sizeof(msg[0]) * idx);
+
+        pthread_t tid;
+        tid = pthread_self();
+        printf("%d puts : ", (int)tid);
+        for(int i = 0 ; i < idx ; i++){
+            printf("%d ", (buf->elem[buf->rear])[i]);
+        }
+        printf("\n");
+        
+        buf->rear = (buf->rear + 1) % buf->capacity ;
+		buf->num += 1 ;
+	pthread_mutex_unlock(&(buf->lock)) ;
+	sem_post(&(buf->filled)) ;
+}
+
+int * bounded_buffer_dequeue(bounded_buffer * buf) 
+{
+	int * r = 0x0 ;
+	sem_wait(&(buf->filled)) ;
+	pthread_mutex_lock(&(buf->lock)) ;
+		
+        buf->front = (buf->front + 1) % buf->capacity ;
+		buf->num -= 1 ;
+
+	pthread_mutex_unlock(&(buf->lock)) ;
+	sem_post(&(buf->empty)) ;
+	return r ;
+}
 
 /* Read line number from given file to figure out the number N */
 int getNcities (char * arg){        
@@ -62,7 +114,7 @@ void sigchldHandler (){                                    // Let main process r
 }
 
 /* Recursively traverse all the possible routes and calculate the length */
-void _travel (int idx){     
+void _travel (int idx, int * visited, int * path){     
     if (idx == size){
         path[idx] = path[0];                                    // Set route from last city to starting city.
 
@@ -81,7 +133,7 @@ void _travel (int idx){
                 path[idx] = i;                                  // Record the order of visiting
                 visited[i] = 1;                                 // Mark as visited
                 length += cities[path[idx-1]][i];               // Add length
-                _travel(idx + 1);                               // Move to the next city
+                _travel(idx + 1, visited, path);                               // Move to the next city
                 length -= cities[path[idx-1]][i];               // Restore length to before visiting the city
                 visited[i] = 0;                                 // Reset the marking
             }       
@@ -90,48 +142,58 @@ void _travel (int idx){
 }
 
 /* Create subtasks, create child process and assign the tasks to child process */
-void subtaskMaker (int idx, int childNumLimit){     
+void subtaskMaker (int idx, int size, int * visited, int * path){     
     /* When prefix of the substask is created. */ 
     if (idx == size-MAX_SUBTASK){ 
-        /* Main process behavior after forking */
-        if ((pid = fork()) > 0){
-            childNum++;                 
-            if (childNum == childNumLimit){         // If the number of child process touches to limit
-                wait(NULL);                         // Wait until any running child process is terminated
-            }
-        }
-        /* forking failed  */
-        if (pid < 0){
-            printf("Fork failed.\n");
-            exit(0);
-        }
-        /* Child process behavior after forking */
-        if (pid == 0){
-            checkedRoute = 0;           // Reset the route counter int
-            min = -1;                   // Reset the min value inherited from main process
-            _travel(idx);               // Start solving the substasks
-            child_proc();               // Write optimal solution from the child process into pipe
-            exit(0);                    // Terminate child process
-        }     
+       while(buf->num == threadLimit);
+        printf("%d\n", buf->num);
+        bounded_buffer_queue(buf, path, idx) ;
     }
     else {
         for (int i = 0 ; i < size ; i++){
             if (visited[i] == 0){                             // Check if the route is already visited
                 path[idx] = i;                                // Record the order of visiting
                 visited[i] = 1;                               // Mark as visited
-                subtaskMaker(idx+1, childNumLimit);           // Move to the next city
+                subtaskMaker(idx+1, size, visited, path);           // Move to the next city
                 visited[i] = 0;                               // Reset the marking
             }       
         }
     }
 }
 
+void * producer_func (void * ptr){
+    int path [51] = {0,};
+    int visited [51] = {0,};            // Mark visited city
+    subtaskMaker(0, size, visited, path);
+    return 0x0;
+}
+
+void * consumer_func (void * ptr){
+    int * prefix; 
+    int path [51] = {0,};
+    int visited [51] = {0,};            // Mark visited city
+    prefix = bounded_buffer_dequeue(buf);
+
+    for (int i = 0 ; i < 5 ; i++){
+        visited[prefix[i]] = 1;
+        path[i] = prefix[i];
+    }
+
+    _travel(size-MAX_SUBTASK+1, visited, path);
+
+    return 0x0;
+}
+
 int main (int argc, char* argv []){
     FILE * fp = fopen (argv[1], "r");
-    int childNumLimit = atoi(argv[2]);                        // Limit number of child process
-    signal(SIGINT, sigintHandler);                            // Set signal handler for SIGINT when occurs interrupt signal detected
-    signal(SIGCHLD, sigchldHandler);                          // Set signal handler for SIGCHLD when occurs child process terminated
+    threadLimit = atoi(argv[2]);                        // Limit number of child process
     
+    pthread_t producer;
+    pthread_t consumer[threadLimit];
+
+    buf = malloc(sizeof(bounded_buffer));
+	bounded_buffer_init(buf, threadLimit);
+
     /* Get number of cities */
     size = getNcities(argv[1]);
 
@@ -143,8 +205,15 @@ int main (int argc, char* argv []){
     }
     fclose(fp);
 
+    pthread_create(&producer, 0x0, producer_func, 0x0);
+    for(int i = 0 ; i < threadLimit ; i++){
+        pthread_create(&(consumer[i]), 0x0, consumer_func, 0x0) ;
+    }
 
+    pthread_join(producer, 0x0) ;
+    for (int i = 0 ; i < threadLimit ; i++) {
+		pthread_join(consumer[i], 0x0) ;
+	}
 
-   
     return 0;
 }
