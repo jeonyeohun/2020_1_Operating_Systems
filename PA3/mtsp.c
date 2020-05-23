66fad588033763486949971d14486391da3358fc
jeonyeohun <21500630@handong.edu>
2020-05-23 16:42:21 +0900

    Update mtsp.c

--- PA3/mtsp.c
+++ PA3/mtsp.c
@@ -12,7 +12,6 @@
 #define MAX_THREADS 8
 #define MAX_CITIES 50
 
-
 int cities[MAX_CITIES + 1][MAX_CITIES + 1]; // Map of city distance
 int minPath[MAX_CITIES + 1] = {0};          // The best path for the shortest distance
 int size;                                   // The total number of cities
@@ -23,6 +22,7 @@ pthread_t producer;
 pthread_t consumer[MAX_THREADS];
 
 long long checkedRoute[MAX_THREADS] = {0}; // Number of checked route by single process
+int isProducerAlive = 0;
 
 typedef struct
 {
@@ -54,10 +54,12 @@ void bounded_buffer_queue(bounded_buffer *buf, int *msg, int idx)
 {
     sem_wait(&(buf->empty));
     pthread_mutex_lock(&(buf->lock));
+
     (buf->elem)[buf->rear] = (int *)malloc(sizeof(msg[0]) * idx);
     memcpy((buf->elem)[buf->rear], msg, sizeof(msg[0]) * idx);
     buf->rear = (buf->rear + 1) % buf->capacity;
     buf->num += 1;
+
     pthread_mutex_unlock(&(buf->lock));
     sem_post(&(buf->filled));
 }
@@ -178,30 +180,33 @@ void subtaskMaker(int idx, int size, int *visited, int *path)
 
 void *producer_func(void *ptr)
 {
-    int path[51] = {
-        0,
-    };
-    int visited[51] = {
-        0,
-    }; // Mark visited city
+    isProducerAlive = 1;
+    int path[51] = {0};
+    int visited[51] = {0};
     subtaskMaker(0, size, visited, path);
+    isProducerAlive = 0;
     return 0x0;
 }
 
 void *consumer_func(void *ptr)
 {
     int idx = *(int *)ptr;
-printf("%d\n", idx);
+    pthread_mutex_t lock;
+    pthread_mutex_init(&lock, 0x0);
     while (1)
     {
         int *prefix;
-        int path[51] = {
-            0,
-        };
-        int visited[51] = {
-            0,
-        };
+        int path[51] = {0};
+        int visited[51] = {0};
         int length = 0;
+
+        pthread_mutex_lock(&(buf->lock));
+        if (!isProducerAlive && buf->num == 0)
+        {
+            pthread_mutex_unlock(&(buf->lock));
+            break;
+        }
+        pthread_mutex_unlock(&(buf->lock));
         prefix = bounded_buffer_dequeue(buf);
 
         for (int i = 0; i < size - MAX_SUBTASK; i++)
