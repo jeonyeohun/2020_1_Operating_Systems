dc89fc3aab623d2d3b8d1f760abf1482009b1edd
jeonyeohun <21500630@handong.edu>
2020-06-04 02:48:54 +0900

    trying new approach by using dfs and adjacency list

--- PA4/ddchck.c
+++ PA4/ddchck.c
@@ -9,132 +9,81 @@
 #include <string.h>
 #include <pthread.h>
 
-int parent [10] = {0,};
-int nodeList_len = 0;
-int level[10] = {0,};
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 
 typedef struct node{
 	unsigned long int ownerThread;
 	int* lockID;
+	struct node* next;
 }Node;
-Node nodeList [10] = {0x0};
 
-int find(int child){
-	if(child == parent[child]) return child;
-	return parent[child] = find(parent[child]);
+Node* nodeList [10] = {0x0};
+int visited[10] = {0};
+
+int nodeCount = 0;
+int targetIdx;
+
+int isNew(int* lid){
+	for (int i = 0 ; i < nodeCount ; i++){
+		if (nodeList[i]->lockID == lid){ 
+			targetIdx = i;
+			return 0;
+		}
+	}
+	return 1;
 }
 
-int merge(int a, int b){
-	a = find(a);
-	b = find(b);
+void addNode(unsigned long int tid, int* lid){
+	Node *head = (Node*)malloc(sizeof(Node));
+	head->ownerThread = tid;
+	head->lockID = lid;
+	head->next = NULL;
+	
+	nodeList[nodeCount] = head;
+	nodeCount++;
+}
 
-	if(a == b) return 1;
+void addEdge(unsigned long int tid, int* lid){
+	Node* curr = (Node*)malloc(sizeof(Node));
+	curr->ownerThread = tid;
+	curr->lockID = lid;
+	curr->next = NULL;
 
-	if (level[a] < level[b]) parent[b] = a;
-	else parent[a] = b;
+	Node* ptr = nodeList[targetIdx];
 
-	if (level[a] == level[b]) level[b]++;
+	while(ptr->next != NULL){
+		ptr = ptr->next;
+	}
+	
+	ptr->next = curr;
+}
 
+void deleteNode(unsigned long int tid, int* lid){
+}
+
+int dfs(int s){
+	visited[s]++;
+
+}
+
+int isCycle(unsigned long int tid, int* lid){
+	for (int i = 0 ; i < nodeCount ; i++){
+		if(dfs(s) == 1) return 1;	
+	}
 	return 0;
 }
 
 
-void addNode(unsigned long int tid, int* lid){
-	for (int i = 0 ; i < nodeList_len  ; i++){
-		if (nodeList[i].lockID == lid) {
-			if(nodeList[i].ownerThread == 0) 
-				nodeList[i].ownerThread = tid;
-			return;
-		}	
-	}
-	nodeList[nodeList_len].ownerThread = tid;
-	nodeList[nodeList_len].lockID = lid;
-	nodeList_len++;
-}
-
-
-void deleteNode(unsigned long int tid, int* lid){
-	int newParent;
-	for (int i = 0 ; i < nodeList_len ; i++){
-		if (nodeList[i].lockID == lid){
-			parent[i] = i;
-			nodeList[i].ownerThread = 0;
-
-			for (int j = 0 ; j < nodeList_len ; j++){
-				if (i!=j && parent[j] == i){
-					newParent = j;			
-				}
-			}
-			for (int j = 0 ; j < nodeList_len ; j++){
-				if (i!=j && parent[j] == i){
-					parent[j] = newParent;	
-				}
-			}
-		}
-	}
-
-}
-
-int isHold(unsigned long int tid, int* lid){
-	for (int i = 0 ; i < nodeList_len ; i++){
-		if (nodeList[i].ownerThread == tid && nodeList[i].lockID != lid) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-
-
 void lock_behavior(unsigned long int tid, int* lid){
-	int target;
-	int cur;
-	addNode(tid, lid);
-	
-	for (int i = 0 ; i < nodeList_len ;i++){
-		if (nodeList[i].lockID == lid){
-			target = i;
-			break;
-		}
-	}
-	
-	if ((cur = isHold(tid, lid)) != -1){
-		if(merge(cur, target)){
-			printf("==== DEADLOCK DETECTED =====\n");
-			printf("Below threads and locks are involved in this deadlock\n");
-			int idx = 1;
-			for (int i = 0 ; i < nodeList_len ; i++){
-				if(parent[cur] == parent[i]){
-					printf("[%d] Thread ID: %lu Lock Address: %p\n", idx++, nodeList[i].ownerThread, nodeList[i].lockID);
-				}
-			}
-		
-		}
-	
-	}
-	for (int i = 0 ; i < 10 ; i++){
-		printf("%d ",parent[i]);
-	}	
-	printf("\n");
+	if(isNew(lid)) addNode(tid, lid);
+	else addEdge(tid, lid);
+}
 
-	for (int i = 0 ; i < nodeList_len ; i ++){
-		printf("%lu ", nodeList[i].ownerThread);
-	}
-	printf("\n");
-	
-	for (int i = 0 ; i < nodeList_len ; i ++){
-		printf("%p ", nodeList[i].lockID);
-	}
-	printf("\n");
-	
+void unlock_behavior(unsigned long int tid, int* lid){
+
 }
 
 int main () {
-	for (int i = 0 ; i < 10 ; i++){
-		parent[i] = i;
-	}
 	int fd = open("channel", O_RDONLY | O_SYNC);
 	while (1) {
 		unsigned long int tid;
@@ -142,7 +91,6 @@ int main () {
 		char buf[128];
 		int len =0;
 		int op;
-		pthread_mutex_lock(&lock);
 		if ((len = read(fd, buf, 128)) == -1)
 			break ;
 		if (len > 0){
@@ -155,7 +103,6 @@ int main () {
 			}
  			
 		} 
-		pthread_mutex_unlock(&lock);
 	}
 	close(fd) ;
 	return 0 ;
