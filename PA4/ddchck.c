3a990054f4380d16edecb22214da61d5f619e89a
jeonyeohun <21500630@handong.edu>
2020-06-04 20:18:32 +0900

    working on making graph with adj list

--- PA4/ddchck.c
+++ PA4/ddchck.c
@@ -7,9 +7,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string.h>
-#include <pthread.h>
-
-
 
 typedef struct node{
 	unsigned long int ownerThread;
@@ -18,19 +15,25 @@ typedef struct node{
 }Node;
 
 Node* nodeList [10] = {0x0};
-int visited[10] = {0};
 
+int visited[10] = {0};
 int nodeCount = 0;
-int targetIdx;
+int targetIdx = -1;
 
 int isNew(int* lid){
 	for (int i = 0 ; i < nodeCount ; i++){
 		if (nodeList[i]->lockID == lid){ 
-			targetIdx = i;
 			return 0;
 		}
 	}
 	return 1;
+}
+
+int searchNode(int *lid ){
+	for (int i = 0 ; i < nodeCount ; i++){
+		if (nodeList[i]->lockID == lid) return i;
+	}
+	return -1;	
 }
 
 void addNode(unsigned long int tid, int* lid){
@@ -43,44 +46,98 @@ void addNode(unsigned long int tid, int* lid){
 	nodeCount++;
 }
 
+int searchThread(unsigned long int tid){
+	for (int i = 0 ; i < nodeCount ; i++){
+		if (nodeList[i]->ownerThread == tid) return i;
+	}
+	return -1;
+}
+
 void addEdge(unsigned long int tid, int* lid){
 	Node* curr = (Node*)malloc(sizeof(Node));
 	curr->ownerThread = tid;
 	curr->lockID = lid;
 	curr->next = NULL;
 
-	Node* ptr = nodeList[targetIdx];
-
-	while(ptr->next != NULL){
-		ptr = ptr->next;
-	}
+	if((targetIdx = searchThread(tid)) > 0){
+		Node* ptr;
+		ptr =  nodeList[targetIdx];
 	
-	ptr->next = curr;
+		while(ptr->next != NULL){
+			ptr = ptr->next;
+		}	
+		
+		ptr->next = curr;
+	}
 }
 
 void deleteNode(unsigned long int tid, int* lid){
+	
 }
+
 
 int dfs(int s){
 	visited[s]++;
+	if(visited[s] > 1) return 1;
+	Node* ptr = nodeList[s];
+
+	while(ptr != NULL){
+		int idx = searchNode(ptr->next->lockID);
+		dfs(idx);
+		ptr = ptr->next;
+	}
+	return 0;
 
 }
 
 int isCycle(unsigned long int tid, int* lid){
+	for (int i = 0 ; i < 10 ; i++){
+		visited[i] = 0;
+	}	
 	for (int i = 0 ; i < nodeCount ; i++){
-		if(dfs(s) == 1) return 1;	
+		if(dfs(i) == 1) return 1;	
 	}
+
 	return 0;
 }
 
 
 void lock_behavior(unsigned long int tid, int* lid){
 	if(isNew(lid)) addNode(tid, lid);
-	else addEdge(tid, lid);
+	else {
+		addEdge(tid, lid);
+		if(isCycle(tid, lid)) printf("deadlock\n");
+	}
 }
 
 void unlock_behavior(unsigned long int tid, int* lid){
+	int delIdx;
+	for (int i = 0 ; i < nodeCount ; i++){
+		if(nodeList[i]->lockID == lid){
+			delIdx = i;	
+		}
+	}
+	for (int i = delIdx ; i < nodeCount-1 ; i++){
+		nodeList[i] = nodeList[i+1];
+	}
+	nodeCount--;
 
+	for(int i = 0 ; i < nodeCount ; i++){
+		Node* curr = nodeList[i];
+		Node* prev = nodeList[i];
+		while(curr != NULL && curr->next->lockID != lid ){
+			curr = curr->next;
+		}
+		
+		if(curr != NULL){
+			while(prev->next == curr){
+				prev = prev->next;
+			}
+		}
+
+		prev->next = curr->next;
+		free(curr);
+	}
 }
 
 int main () {
@@ -99,9 +156,18 @@ int main () {
 				lock_behavior(tid, lid);
 			}
 			else{
-				deleteNode(tid, lid);
+//				unlock_behavior(tid, lid);
 			}
- 			
+			printf("\nop: %d tid: %lu, lid: %p\n", op, tid, lid);
+			for(int i = 0 ; i < nodeCount ; i++){
+				printf("%p(%lu) -> ", nodeList[i]->lockID, nodeList[i]->ownerThread);
+				Node* ptr = nodeList[i]->next;
+				while(ptr != NULL){
+					printf("%p(%lu) -> ", ptr->lockID, ptr->ownerThread);
+					ptr = ptr->next;
+				}
+				printf("NULL\n");
+			}	
 		} 
 	}
 	close(fd) ;
